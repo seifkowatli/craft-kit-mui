f0978f6fbb95a51aff8d6bdabe03cd704d9590ef
SEIF <seif.kowatli@hotmail.com>
2024-02-29 17:20:07 +0400

    amending the main export to include providers , helpers and utils

---
+++ .changeset/serious-swans-poke.md
@@ -0,0 +1,5 @@
+---
+"craft-kit-mui": patch
+---
+
+export providers ,helpers and utils to the main package export
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
 export * from './components'
+export * from './helpers'
 export * from './hooks'
+export * from './providers'
+export * from './utils'
+
